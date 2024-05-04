08ed18d56c621fc1d730111ad4ac7e2744157fd2
Alvi399 <muhammadalvi.23339@mhs.unesa.ac.id>
2024-05-04 15:13:59 +0700

    beta 1

--- Dashboard.py
+++ Dashboard.py
@@ -1,101 +1,138 @@
 import streamlit as st
 import joblib 
 tf_random_forest = joblib.load('./rf_classification_model.pkl')
+    
 
-# Judul Dashboard
-st.title('Input Preferensi Pengguna')
+#mebuat footer
 
-# Pilihan untuk Favorite Color
-favorite_color = st.selectbox('Pilih Warna Favorit:', ['Cool', 'Neutral', 'Warm'])
+def main():
+    st.sidebar.title("Navigasi")
+    page = st.sidebar.radio("Pergi ke", ("Beranda", "Pohon 1", "Pohon 2", "Pohon 3", "Pohon 4", "Pohon 5","Credit"))
 
-# Pilihan untuk Favorite Music Genre
-favorite_genre = st.selectbox('Pilih Genre Musik Favorit:', ['Rock', 'Hip hop', 'Folk/Traditional', 'Jazz/Blues', 'Pop', 'Electronic', 'R&B and soul'])
+    # Konten berdasarkan navigasi
+    if page == "Beranda":      
+    # Judul Dashboard
+        st.title('Input Preferensi Pengguna')
 
-# Pilihan untuk Favorite Beverage
-favorite_beverage = st.selectbox('Pilih Minuman Favorit:', ['Vodka', 'Wine', 'Whiskey', 'Doesn\'t drink', 'Beer', 'Other'])
+        # Pilihan untuk Favorite Color
+        favorite_color = st.selectbox('Pilih Warna Favorit:', ['Cool', 'Neutral', 'Warm'])
 
-# Pilihan untuk Favorite Soft Drink
-favorite_soft_drink = st.selectbox('Pilih Minuman Ringan Favorit:', ['7UP/Sprite', 'Coca Cola/Pepsi', 'Fanta', 'Other'])
+        # Pilihan untuk Favorite Music Genre
+        favorite_genre = st.selectbox('Pilih Genre Musik Favorit:', ['Rock', 'Hip hop', 'Folk/Traditional', 'Jazz/Blues', 'Pop', 'Electronic', 'R&B and soul'])
+
+        # Pilihan untuk Favorite Beverage
+        favorite_beverage = st.selectbox('Pilih Minuman Favorit:', ['Vodka', 'Wine', 'Whiskey', 'Doesn\'t drink', 'Beer', 'Other'])
+
+        # Pilihan untuk Favorite Soft Drink
+        favorite_soft_drink = st.selectbox('Pilih Minuman Ringan Favorit:', ['7UP/Sprite', 'Coca Cola/Pepsi', 'Fanta', 'Other'])
 
 
-# Tombol untuk submit
-submitted = st.button('Submit')
+        # Tombol untuk submit
+        submitted = st.button('Submit')
 
-st.subheader('Hasil Prediksi:')
-# Tampilkan hasil jika tombol submit ditekan
-if submitted:
-    st.write('Preferensi Pengguna:')
-    st.write(f'- Warna Favorit: {favorite_color}')
-    st.write(f'- Genre Musik Favorit: {favorite_genre}')
-    st.write(f'- Minuman Favorit: {favorite_beverage}')
-    st.write(f'- Minuman Ringan Favorit: {favorite_soft_drink}')
+        st.subheader('Hasil Prediksi:')
+        # Tampilkan hasil jika tombol submit ditekan
+        if submitted:
+            st.write('Preferensi Pengguna:')
+            st.write(f'- Warna Favorit: {favorite_color}')
+            st.write(f'- Genre Musik Favorit: {favorite_genre}')
+            st.write(f'- Minuman Favorit: {favorite_beverage}')
+            st.write(f'- Minuman Ringan Favorit: {favorite_soft_drink}')
 
-    # Konversi pilihan warna favorit ke nilai numerik
-    if favorite_color == 'Cool':
-        favorite_color = 0.0
-    elif favorite_color == 'Neutral':
-        favorite_color = 0.5
-    elif favorite_color == 'Warm':
-        favorite_color = 1.0
-    else:
-        favorite_color = 0.0
+            # Konversi pilihan warna favorit ke nilai numerik
+            if favorite_color == 'Cool':
+                favorite_color = 0.0
+            elif favorite_color == 'Neutral':
+                favorite_color = 0.5
+            elif favorite_color == 'Warm':
+                favorite_color = 1.0
+            else:
+                favorite_color = 0.0
 
-    # Konversi pilihan genre musik favorit ke nilai numerik
-    if favorite_genre == 'Rock':
-        favorite_genre = 1.0
-    elif favorite_genre == 'Hip hop':
-        favorite_genre = 0.3333333333333333
-    elif favorite_genre == 'Folk/Traditional':
-        favorite_genre = 0.16666666666666666
-    elif favorite_genre == 'Jazz/Blues':
-        favorite_genre = 0.5
-    elif favorite_genre == 'Pop':
-        favorite_genre = 0.6666666666666666
-    elif favorite_genre == 'Electronic':
-        favorite_genre = 0.0
-    elif favorite_genre == 'R&B and soul':
-        favorite_genre = 0.8333333333333333
-    else:
-        favorite_genre = 0.0
+            # Konversi pilihan genre musik favorit ke nilai numerik
+            if favorite_genre == 'Rock':
+                favorite_genre = 1.0
+            elif favorite_genre == 'Hip hop':
+                favorite_genre = 0.3333333333333333
+            elif favorite_genre == 'Folk/Traditional':
+                favorite_genre = 0.16666666666666666
+            elif favorite_genre == 'Jazz/Blues':
+                favorite_genre = 0.5
+            elif favorite_genre == 'Pop':
+                favorite_genre = 0.6666666666666666
+            elif favorite_genre == 'Electronic':
+                favorite_genre = 0.0
+            elif favorite_genre == 'R&B and soul':
+                favorite_genre = 0.8333333333333333
+            else:
+                favorite_genre = 0.0
 
-    # Konversi pilihan minuman favorit ke nilai numerik
-    if favorite_beverage == 'Vodka':
-        favorite_beverage =  0.6000000000000001
-    elif favorite_beverage == 'Wine':
-        favorite_beverage = 1.0
-    elif favorite_beverage == 'Whiskey':
-        favorite_beverage = 0.8 
-    elif favorite_beverage == "Doesn't drink":
-        favorite_beverage = 0.2 
-    elif favorite_beverage == 'Beer':
-        favorite_beverage = 0.0 
-    elif favorite_beverage == 'Other':
-        favorite_beverage = 0.4
-    else:
-        favorite_beverage = 0.0
+            # Konversi pilihan minuman favorit ke nilai numerik
+            if favorite_beverage == 'Vodka':
+                favorite_beverage =  0.6000000000000001
+            elif favorite_beverage == 'Wine':
+                favorite_beverage = 1.0
+            elif favorite_beverage == 'Whiskey':
+                favorite_beverage = 0.8 
+            elif favorite_beverage == "Doesn't drink":
+                favorite_beverage = 0.2 
+            elif favorite_beverage == 'Beer':
+                favorite_beverage = 0.0 
+            elif favorite_beverage == 'Other':
+                favorite_beverage = 0.4
+            else:
+                favorite_beverage = 0.0
 
-    # Konversi pilihan minuman ringan favorit ke nilai numerik
-    if favorite_soft_drink == '7UP/Sprite':
-        favorite_soft_drink = 0.0
-    elif favorite_soft_drink == 'Coca Cola/Pepsi':
-        favorite_soft_drink = 0.3333333333333333
-    elif favorite_soft_drink == 'Fanta':
-        favorite_soft_drink = 0.6666666666666666
-    elif favorite_soft_drink == 'Other':
-        favorite_soft_drink = 1.0
-    else:
-        favorite_soft_drink = 0.0
+            # Konversi pilihan minuman ringan favorit ke nilai numerik
+            if favorite_soft_drink == '7UP/Sprite':
+                favorite_soft_drink = 0.0
+            elif favorite_soft_drink == 'Coca Cola/Pepsi':
+                favorite_soft_drink = 0.3333333333333333
+            elif favorite_soft_drink == 'Fanta':
+                favorite_soft_drink = 0.6666666666666666
+            elif favorite_soft_drink == 'Other':
+                favorite_soft_drink = 1.0
+            else:
+                favorite_soft_drink = 0.0
 
-    # Prediksi preferensi pengguna menggunakan model random forest
-    prediction = tf_random_forest.predict([[favorite_color, favorite_genre, favorite_beverage, favorite_soft_drink]])
-    accuracy = tf_random_forest.score([[favorite_color, favorite_genre, favorite_beverage, favorite_soft_drink]], prediction)
-    gender = ""
-    if prediction[0] == 1:
-        gender = "Laki-laki"
-    else:
-        gender = "Perempuan"
-    st.write(f'Prediksi Gender: {gender}')
-    st.write(f'Accuracy: {accuracy}')
-    st.write(f'favorite_bevarage: {favorite_beverage}')
+            # Prediksi preferensi pengguna menggunakan model random forest
+            prediction = tf_random_forest.predict([[favorite_color, favorite_genre, favorite_beverage, favorite_soft_drink]])
+            accuracy = tf_random_forest.score([[favorite_color, favorite_genre, favorite_beverage, favorite_soft_drink]], prediction)
+            gender = ""
+            if prediction[0] == 1:
+                gender = "Laki-laki"
+            else:
+                gender = "Perempuan"
+            st.write(f'Prediksi Gender: {gender}')
+            st.write(f'Accuracy: {accuracy}')
+    elif page == "Pohon 1":
+        st.header("Pohon 1")
+        st.image("./Tree/RandomForestTree_0.png.png", caption="struktur pohon dari algoritma decision tree", use_column_width=True)
+    elif page == "Pohon 2":
+        st.header("Pohon  2")
+        st.image("./Tree/RandomForestTree_1.png.png", caption="struktur pohon dari algoritma decision tree", use_column_width=True)
+    elif page == "Pohon 3":
+        st.header("Pohon  3")
+        st.image("./Tree/RandomForestTree_2.png.png", caption="struktur pohon dari algoritma decision tree", use_column_width=True)
+    elif page == "Pohon 4":
+        st.header("Pohon  4")
+        st.image("./Tree/RandomForestTree_3.png.png", caption="struktur pohon dari algoritma decision tree", use_column_width=True)
+    elif page == "Pohon 5":
+        st.header("Pohon  5")
+        st.image("./Tree/RandomForestTree_4.png.png", caption="struktur pohon dari algoritma decision tree", use_column_width=True)
+    elif page == "Credit":
+        st.header("Anggota Kelompok")
+        st.write("1. Muhammad Alvi Kirana Zulfan Nazal (23051204339)")
+        st.write("2. Mochamad Fajar Krisdianto (23051204327)")
+        st.write("3. Nofi Ayu Wulandari (23051204326)")
+        st.write("4. Khoerunnisa Utami (23051204340)")
 
+    st.markdown("""
+    ---
+    © 2024 Aplikasi Kami | Dibuat dengan ❤️ oleh Kami \n
+    Support system by Allah SWT
+    """)
+
+if __name__ == "__main__":
+    main()
 
---
+++ Readme.md
@@ -0,0 +1,45 @@
+# Deteksi Gender
+
+## Description
+proyek ini dibuat untuk membatu mnggimplemntasikan materi materi diskrit yang telah dipelajari salah satu materinya adalah algoritma Random Forest yang digunakan untuk mengklasiifikasi data dan Algoritma Decision tree yang digunakan untuk mengklasifikasi data berdasarkan aturan aturan yang telah dibuat
+
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Features](#features)
+- [License](#license)
+
+## Installation
+langkah pertama yang harus dilakukan adalah menginstall python di komputer anda, jika sudah terinstall python maka langkah selanjutnya adalah menginstall library yang dibutuhkan dengan cara membuka terminal dan menjalankan perintah berikut
+
+```bash
+pip install -r requirements.txt
+```
+
+
+## Usage
+cara menggunakan proyek ini adalah dengan menjalankan file dashboard.py dengan cara membuka terminal dan menjalankan perintah berikut
+
+```bash
+streamlit run dashboard.py
+```
+
+
+## Features
+Progrm ini memiliki beberapa fitur menu yaitu
+1. bernda
+2. pohon 1
+3. pohon 2
+4. pohon 3
+5. pohon 4
+6. pohon 5
+7. credit
+
+dari semua fitur menu tersebut kita bisa melihat hasil dari algoritma Random Forest dan Decision Tree yang telah dibuat oleh kelompok kami
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
+## How to Get Help
+Contact me on my email: [Email](mailto:muhammadali.23339@mhs.unesa.ac.id)
